6385ee3a3d780a91fdf3857ddedc9c6ef6617181
Moneem saadaoui <contact.moneem@gmail.com>
2017-11-25 16:24:15 +0100

    Initial commit

---
+++ app/src/main/java/rrdl/gdg/Loginpage.java
@@ -0,0 +1,13 @@
+package rrdl.gdg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Loginpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginpage);
+    }
+}
